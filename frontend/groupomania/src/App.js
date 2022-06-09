961f121114eb1f84ac6aea96ec1422b60ba702dd
Thomas62970 <toto-62970@live.fr>
2022-06-09 06:33:23 +0200

    début frontend

---
+++ frontend/groupomania/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Connection from './pages/Connection';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Connection />
+    </div>
+  );
+}
+
+export default App;
